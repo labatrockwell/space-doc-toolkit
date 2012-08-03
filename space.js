f2c49a3e8fa3fe95ecab5db7cdb5f5f0aee15920
Joshua Walton <adenine@gmail.com>
2012-08-03 18:37:18 -0400

    Changed index.html and json

--- space.js
+++ space.js
@@ -1,95 +1,97 @@
 {
-  "spime": {
-    "-name": "LED Sign",
-    "description": "sdaklfjsdfjsdlkfdf ",
-    "publish": {
-      "message": [
+    "spimes": [
         {
-          "-name": "partyModeOn",
-          "-type": "boolean",
-          "#default": "false"
+            "spime": {
+                "-name": "LED Sign",
+                "description": "sdaklfjsdfjsdlkfdf ",
+                "publish": {
+                    "message": [
+                        {
+                            "-name": "partyModeOn",
+                            "-type": "boolean",
+                            "#default": "false"
+                        },
+                        {
+                            "-name": "currentColor",
+                            "-type": "number",
+                            "#default": "1023"
+                        },
+                        {
+                            "-name": "currentMessage",
+                            "-type": "string",
+                            "#default": "not-updated"
+                        }
+                    ]
+                },
+                "subscribe": {
+                    "message": [
+                        {
+                            "-name": "partyMode",
+                            "-type": "boolean"
+                        },
+                        {
+                            "-name": "color",
+                            "-type": "number"
+                        },
+                        {
+                            "-name": "text",
+                            "-type": "string"
+                        }
+                    ]
+                }
+            }
         },
         {
-          "-name": "currentColor",
-          "-type": "number",
-          "#default": "1023"
+            "spime": {
+                "-name": "Button",
+                "description": "sdaklfjsdfjsdlkfdf ",
+                "publish": {
+                    "message": [
+                        {
+                            "-name": "buttonPress",
+                            "-type": "boolean",
+                            "#default": "false"
+                        },
+                        {
+                            "-name": "numberPresses",
+                            "-type": "number",
+                            "#default": "0"
+                        },
+                        {
+                            "-name": "hashtag",
+                            "-type": "string",
+                            "#default": "#pressed"
+                        }
+                    ]
+                }
+            }
         },
         {
-          "-name": "currentMessage",
-          "-type": "string",
-          "#default": "not-updated"
+            "spime": {
+                "-name": "Nerf Gun",
+                "description": "sdaklfjsdfjsdlkfdf ",
+                "publish": {
+                    "message": [
+                        {
+                            "-name": "hasAmmo",
+                            "-type": "boolean",
+                            "#default": "false"
+                        }
+                    ]
+                },
+                "subscribe": {
+                    "message": [
+                        {
+                            "-name": "fire",
+                            "-type": "boolean"
+                        },
+                        {
+                            "-name": "killingword",
+                            "-type": "string"
+                        }
+                    ]
+                }
+            }
         }
-      ]
-    },
-    "subscribe": {
-      "message": [
-        {
-          "-name": "partyMode",
-          "-type": "boolean"
-        },
-        {
-          "-name": "color",
-          "-type": "number"
-        },
-        {
-          "-name": "text",
-          "-type": "string"
-        }
-      ]
-    }
-  }
-}
-
-{
-  "spime": {
-    "-name": "Button",
-    "description": "sdaklfjsdfjsdlkfdf ",
-    "publish": {
-      "message": [
-        {
-          "-name": "buttonPress",
-          "-type": "boolean",
-          "#default": "false"
-        },
-        {
-          "-name": "numberPresses",
-          "-type": "number",
-          "#default": "0"
-        },
-        {
-          "-name": "hashtag",
-          "-type": "string",
-          "#default": "#pressed"
-        }
-      ]
-    }
-  }
-}
-
-{
-  "spime": {
-    "-name": "Nerf Gun",
-    "description": "sdaklfjsdfjsdlkfdf ",
-    "publish": {
-      "message": [
-        {
-          "-name": "hasAmmo",
-          "-type": "boolean",
-          "#default": "false"
-        }
-      ]
-    },
-    "subscribe": {
-      "message": [
-        {
-          "-name": "fire",
-          "-type": "boolean"
-        },
-        {
-          "-name": "killingword",
-          "-type": "string"
-        }
-      ]
-    }
-  }
+    ]
 }
